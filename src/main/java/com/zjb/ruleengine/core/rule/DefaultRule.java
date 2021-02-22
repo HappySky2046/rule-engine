32e95e0b889d19f62645fd0159e6278efe1550e1
zhaojingbo <zhaojingbo>
2021-02-22 14:23:48 +0800

    日志

---
+++ src/main/java/com/zjb/ruleengine/core/rule/DefaultRule.java
@@ -0,0 +1,25 @@
+package com.zjb.ruleengine.core.rule;
+
+import com.zjb.ruleengine.core.Context;
+import com.zjb.ruleengine.core.value.Value;
+
+/**
+ * @author 赵静波 <wb_zhaojingbo@kuaishou.com>
+ * Created on 2021-01-02
+ * 没有条件的规则，规则永远执行action并返回结果
+ */
+public class DefaultRule extends AbstractRule {
+    @Override
+    public Boolean executeCondition(Context context) {
+        return true;
+    }
+
+    public DefaultRule(String id,  Value action) {
+        super(id, null, action);
+    }
+
+    @Override
+    public void build() {
+
+    }
+}
